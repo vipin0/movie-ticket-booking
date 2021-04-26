c9ee2acb650ff6621f1911b91c5d7e3bbd21b6a7
vipin0 <vipin6673@gmail.com>
2021-04-26 09:08:08 +0530

    postgresql

--- movie_booking/settings.py
+++ movie_booking/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'movie',
     'booking',
     'crispy_forms',
-    
+
 ]
 
 MIDDLEWARE = [
@@ -81,13 +81,23 @@ WSGI_APPLICATION = 'movie_booking.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'movie',
+        'USER': 'postgres',
+        'PASSWORD': 'vipin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
